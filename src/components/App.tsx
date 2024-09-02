7bfdadc62dc40e252d2409b008d9d0c536ca11a5
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-09-02 23:49:09 +0300

    stop automatic scroll on button click

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,5 @@
 import List from "./list/List";
-import ToggleScrollButton from "./toggleScrollButton/ToggleScrollButton";
 
 export default function App() {
-  return (
-    <>
-      <List />
-      <ToggleScrollButton />
-    </>
-  );
+  return <List />;
 }
--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useRef } from "react";
 import { FixedSizeList } from "react-window";
 import { useFetch } from "../../hooks/useFetch";
+import { useScrollToggle } from "../../hooks/useScrollToggle";
 import "./List.css";
 
 export default function List() {
   const { logsState } = useFetch();
+  const { isAutomaticScroll, toggleScroll, action } = useScrollToggle();
   const listRef = useRef<FixedSizeList>(null);
 
   useEffect(() => {
-    if (logsState.isConnected && listRef.current) {
+    if (logsState.isConnected && isAutomaticScroll && listRef.current) {
       listRef.current.scrollToItem(logsState.logs.length - 1);
     }
-  }, [logsState]);
+  }, [logsState, isAutomaticScroll]);
 
   const ListItem = ({
     index,
@@ -28,15 +30,24 @@ export default function List() {
   };
 
   return logsState.isConnected ? (
-    <FixedSizeList
-      ref={listRef}
-      height={820}
-      itemCount={logsState.logs.length}
-      itemSize={40}
-      width="100%"
-    >
-      {ListItem}
-    </FixedSizeList>
+    <>
+      <FixedSizeList
+        ref={listRef}
+        height={820}
+        itemCount={logsState.logs.length}
+        itemSize={40}
+        width="100%"
+      >
+        {ListItem}
+      </FixedSizeList>
+      <button
+        className="toggle"
+        style={{ backgroundColor: `${isAutomaticScroll ? "green" : "red"}` }}
+        onClick={toggleScroll}
+      >
+        {action}
+      </button>
+    </>
   ) : (
     <p>Unable to connect to Web Socket</p>
   );
